f0e3ec62aac106506f30bf4ba76cafbbbc5ebcce
lucadln <lucyan.ardelean@gmail.com>
2019-10-17 18:39:25 +0300

    Add some of my recent work

---
+++ typescript/greeter.js
@@ -0,0 +1,14 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " M. " + person.lastName;
+}
+var user = { firstName: "Lucian", lastName: "Ardelean" };
+document.body.textContent = greeter(user);
---
+++ w3schools-tutorial/modules/myCtrl2.js
@@ -0,0 +1,4 @@
+app2.controller("myCtrl2", function($scope) {
+	$scope.firstName = "Lucian";
+	$scope.lastName = "Ardelean";
+});
